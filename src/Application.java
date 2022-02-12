44bfbc509a63b0ee2ec1b1c06bdc8a3f1106fcc3
Emmanuel <eolmedo@gmail.com>
2022-02-12 10:45:52 -0300

    expresiones lambda

--- src/Application.java
+++ src/Application.java
@@ -1,8 +1,22 @@
-public class Application {
+public class Application implements ApplicationImpl{
 
-    
+    private String nombre;
+    private String apellido;
+
+    public Application() {
+        nombre = "Emmanuel";
+        apellido = "Olmedo";
+    }
+
+    @Override
+    public void consumidoras() {
+  
+    }
 
     public static void main(String[] args) {
-
+        Application app = new Application();
+        System.out.println(app.nombre +" "+app.apellido);
     }
+
+
 }
